71c03eb248dcccbad3ba88e60dd1564697ce0323
23051230-netizen <23051230@vnu.edu.vn>
2026-05-29 10:26:30 +0700

    Update App.py

--- App.py
+++ App.py
@@ -14,7 +14,6 @@ import os, io
 import numpy as np
 import pandas as pd
 import streamlit as st
-import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
